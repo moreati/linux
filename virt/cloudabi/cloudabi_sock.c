fbc2c09a9022a76d664b35cd57a740d4180ba136
Ed Schouten <ed@nuxi.nl>
2016-02-23 14:29:06 +0100

    Make getpeername(), getsockname(), getsockopt(), etc. mostly work.

--- virt/cloudabi/cloudabi_sock.c
+++ virt/cloudabi/cloudabi_sock.c
@@ -23,13 +23,44 @@
  * SUCH DAMAGE.
  */
 
+#include <linux/in.h>
+#include <linux/in6.h>
 #include <linux/namei.h>
 #include <linux/net.h>
 #include <linux/syscalls.h>
 
+#include <net/sock.h>
+
 #include "cloudabi_syscalldefs.h"
 #include "cloudabi_syscalls.h"
 #include "cloudabi_util.h"
+
+static void cloudabi_convert_sockaddr(const struct sockaddr_storage *ss,
+                                      cloudabi_sockaddr_t *rsa)
+{
+	const struct sockaddr_in *sin;
+	const struct sockaddr_in6 *sin6;
+
+	switch (ss->ss_family) {
+	case AF_INET:
+		sin = (const struct sockaddr_in *)ss;
+		rsa->sa_family = CLOUDABI_AF_INET;
+		memcpy(&rsa->sa_inet.addr, &sin->sin_addr,
+		    sizeof(rsa->sa_inet.addr));
+		rsa->sa_inet.port = ntohs(sin->sin_port);
+		return;
+	case AF_INET6:
+		sin6 = (const struct sockaddr_in6 *)ss;
+		rsa->sa_family = CLOUDABI_AF_INET6;
+		memcpy(&rsa->sa_inet6.addr, &sin6->sin6_addr,
+		    sizeof(rsa->sa_inet6.addr));
+		rsa->sa_inet6.port = ntohs(sin6->sin6_port);
+		return;
+	case AF_UNIX:
+		rsa->sa_family = CLOUDABI_AF_UNIX;
+		return;
+	}
+}
 
 cloudabi_errno_t cloudabi_sys_sock_accept(
     const struct cloudabi_sys_sock_accept_args *uap, unsigned long *retval)
@@ -45,13 +76,12 @@ cloudabi_errno_t cloudabi_sys_sock_bind(
 	struct path path;
 	struct dentry *dentry;
 	struct socket *sock;
-	int err;
+	int err = 0;
 
 	cap_rights_init(&rights, CAP_BIND);
 	f_sock = fdget_raw_rights(uap->s, &rights);
 	if (IS_ERR(f_sock.file))
 		return cloudabi_convert_errno(PTR_ERR(f_sock.file));
-
 	sock = sock_from_file(f_sock.file, &err);
 	if (sock == NULL)
 		goto out;
@@ -79,13 +109,12 @@ cloudabi_errno_t cloudabi_sys_sock_connect(
 	struct fd f_sock;
 	struct path path;
 	struct socket *sock;
-	int err;
+	int err = 0;
 
 	cap_rights_init(&rights, CAP_CONNECT);
 	f_sock = fdget_raw_rights(uap->s, &rights);
 	if (IS_ERR(f_sock.file))
 		return cloudabi_convert_errno(PTR_ERR(f_sock.file));
-
 	sock = sock_from_file(f_sock.file, &err);
 	if (sock == NULL)
 		goto out;
@@ -131,5 +160,39 @@ cloudabi_errno_t cloudabi_sys_sock_shutdown(
 cloudabi_errno_t cloudabi_sys_sock_stat_get(
     const struct cloudabi_sys_sock_stat_get_args *uap, unsigned long *retval)
 {
-	return CLOUDABI_ENOSYS;
+	cloudabi_sockstat_t ss = {};
+	struct capsicum_rights rights;
+	struct fd f_sock;
+	struct sockaddr_storage address;
+	struct socket *sock;
+	int err = 0, len;
+
+	cap_rights_init(&rights,
+	                CAP_GETSOCKOPT, CAP_GETPEERNAME, CAP_GETSOCKNAME);
+	f_sock = fdget_raw_rights(uap->fd, &rights);
+	if (IS_ERR(f_sock.file))
+		return cloudabi_convert_errno(PTR_ERR(f_sock.file));
+	sock = sock_from_file(f_sock.file, &err);
+	if (sock == NULL)
+		goto out;
+
+	/* Fill ss_sockname and ss_peername. */
+	if (sock->ops->getname(sock, (struct sockaddr *)&address, &len, 0) == 0)
+		cloudabi_convert_sockaddr(&address, &ss.ss_sockname);
+	if (sock->ops->getname(sock, (struct sockaddr *)&address, &len, 1) == 0)
+		cloudabi_convert_sockaddr(&address, &ss.ss_peername);
+
+	/* Fill ss_error. */
+	ss.ss_error = cloudabi_convert_errno(sock_error(sock->sk));
+
+	/* Fill ss_state. */
+	if (sock->sk->sk_state == TCP_LISTEN)
+		ss.ss_state |= CLOUDABI_SOCKSTAT_ACCEPTCONN;
+
+	if (copy_to_user(uap->buf, &ss, sizeof(ss)) != 0)
+		err = -EFAULT;
+
+out:
+	fdput(f_sock);
+	return cloudabi_convert_errno(err);
 }
